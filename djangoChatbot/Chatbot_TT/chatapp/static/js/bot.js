04304bb2afa33402e4795870a1b22084e4a4447f
Anurag Nigam <anuragnigamse0012@gmail.com>
2019-01-26 16:48:51 +0100

    changes for the UI

--- djangoChatbot/Chatbot_TT/chatapp/static/js/bot.js
+++ djangoChatbot/Chatbot_TT/chatapp/static/js/bot.js
@@ -4,7 +4,7 @@ $('#message').keypress(function() {
 	if (event.which == 13) {
 
 		var userinput = $('#message').val();
-		var res = $('<div>');
+		var res = $('<div class="form-group" style="margin:0 0 0 50px;color:steelblue;font-weight:bolder;">');
 		res.text('You: ' + userinput);
 		res.appendTo(text);
 		$('#message').val('');
@@ -20,7 +20,7 @@ $('#message').keypress(function() {
 		method: 'POST',
 		success: function(data) {
 			//What to do when getting the data back from views
-			var newres = $('<div>');
+			var newres = $('<div style="margin:0 0 0 250px;color:fuchsia;font-weight:bolder;">');
 			newres.text('Bot: ' + data['reply']);
 			newres.appendTo(text);
 		}
