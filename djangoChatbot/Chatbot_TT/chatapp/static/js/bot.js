bdaadc9e33fb61dd9ff8202789e1d289fd824793
Anurag Nigam <anuragnigamse0012@gmail.com>
2019-01-26 19:17:02 +0100

    Changes for container for messages

--- djangoChatbot/Chatbot_TT/chatapp/static/js/bot.js
+++ djangoChatbot/Chatbot_TT/chatapp/static/js/bot.js
@@ -4,8 +4,8 @@ $('#message').keypress(function() {
 	if (event.which == 13) {
 
 		var userinput = $('#message').val();
-		var res = $('<div class="form-group" style="margin:0 0 0 50px;color:steelblue;font-weight:bolder;">');
-		res.text('You: ' + userinput);
+		var res = $('<div class="container" style="margin:15px 0 19px 27px;background-color: azure;display: block;clear:both;width: 59%;color:steelblue;font-weight:bolder;padding:8px">');
+		res.text('You: ' +userinput);
 		res.appendTo(text);
 		$('#message').val('');
 
@@ -20,7 +20,7 @@ $('#message').keypress(function() {
 		method: 'POST',
 		success: function(data) {
 			//What to do when getting the data back from views
-			var newres = $('<div style="margin:0 0 0 250px;color:fuchsia;font-weight:bolder;">');
+			var newres = $('<div class="container" style="margin:0 0 0 185px;width: 59%; background-color: antiquewhite;color:fuchsia;font-weight:bolder;padding:8px;">');
 			newres.text('Bot: ' + data['reply']);
 			newres.appendTo(text);
 		}
