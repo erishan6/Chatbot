bf07b36ede8ce1c0dd6a11d75ab61b8a83020bc3
Ishan Adhaulia <erishan6@gmail.com>
2019-02-24 00:13:09 +0100

    Repo refactoring

--- textchat/chatbot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('', views.index, name='index'),
-	path('temp/', views.temp, name='temp'),
-	path('citybot/',views.citybot, name = 'citybot')
-]
