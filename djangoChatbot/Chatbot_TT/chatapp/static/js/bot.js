eadd50ef3d769a657bfca4db5fea587a1d6f43f3
Anurag Nigam <anuragnigamse0012@gmail.com>
2019-02-28 17:16:02 +0100

    adding changes for the submit button

--- djangoChatbot/Chatbot_TT/chatapp/static/js/bot.js
+++ djangoChatbot/Chatbot_TT/chatapp/static/js/bot.js
@@ -1,7 +1,43 @@
 // JS for handling UI for chatbot
+
+function clickme() {
+ alert("jii");
+
+}
 var text= $('#chatcontent');
 $('#message').keypress(function() {
 	if (event.which == 13) {
+
+		var userinput = $('#message').val();
+		var res = $('<div style="margin:15px 0 19px 27px;background-color: azure;display: block;clear:both;width: 59%;color:steelblue;font-weight:bolder;padding:8px; position: fixed;">');
+		res.text('You: ' +userinput);
+		res.appendTo(text);
+		$('#message').val('');
+
+		//Get the response from views and db
+		$.ajax( {
+
+		url: '/chatapp/bot/',
+		type : 'POST',
+		data: {
+			'input': userinput,
+		},
+		method: 'POST',
+		success: function(data) {
+			//What to do when getting the data back from views
+			var newres = $('<div  style="margin:0 0 0 185px;width: 59%; background-color: antiquewhite;color:indigo;font-weight:bolder;padding:8px; position: fixed;">');
+			newres.text('Bot: ' + data['reply']);
+			newres.appendTo(text);
+		}
+		})
+
+	}
+})
+
+
+
+$('#clickMe').click(function() {
+        alert("jelo");
 
 		var userinput = $('#message').val();
 		var res = $('<div class="container" style="margin:15px 0 19px 27px;background-color: azure;display: block;clear:both;width: 59%;color:steelblue;font-weight:bolder;padding:8px">');
@@ -26,5 +62,5 @@ $('#message').keypress(function() {
 		}
 		})
 
-	}
+
 })
